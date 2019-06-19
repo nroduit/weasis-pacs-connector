5dfc39e931f566d8f4a6706c101af0f12763e8c5
btja <btja@192.168.0.148>
2019-06-19 10:22:08 +0200

    comment launch process

--- src/main/java/org/weasis/servlet/GetWeasisProtocol.java
+++ src/main/java/org/weasis/servlet/GetWeasisProtocol.java
@@ -1,12 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 2014 Weasis Team.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * Copyright (c) 2014 Weasis Team. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Nicolas Roduit - initial API and implementation
+ * Contributors: Nicolas Roduit - initial API and implementation
  *******************************************************************************/
 
 package org.weasis.servlet;
@@ -73,6 +69,9 @@ public class GetWeasisProtocol extends HttpServlet {
             }
 
             ConnectorProperties props = connectorProperties.getResolveConnectorProperties(request);
+
+            // BUILD WADO MANIFEST FROM WORKERTHREAD AND GET URL TO RETRIEVE IT LATER
+
             ManifestBuilder builder;
             if (manifest == null) {
                 builder = ServletUtil.buildManifest(request, props);
@@ -81,6 +80,8 @@ public class GetWeasisProtocol extends HttpServlet {
             }
             String wadoQueryUrl = ServletUtil.buildManifestURL(request, builder, props, true);
             wadoQueryUrl = response.encodeRedirectURL(wadoQueryUrl);
+
+            // ADD WADO MANIFEST PARAMETER >> $dicom:get -w "..."
 
             StringBuilder buf = new StringBuilder();
             int startIndex = wadoQueryUrl.indexOf(':');
@@ -92,27 +93,43 @@ public class GetWeasisProtocol extends HttpServlet {
             buf.append(wadoQueryUrl);
             buf.append("\"");
 
-            String urlCfg = request.getParameter(WeasisConfig.PARAM_CONFIG_URL);
-            if (urlCfg == null) {
-                urlCfg = props.getProperty(SERVICE_CONFIG);
-            }
+            // ADD WEASISCONFIG PARAMETERS >> $weasis:config "..."
 
             buf.append(" $weasis:config");
-            if (urlCfg == null) {
-                addElementWithNullValue(buf, WeasisConfig.PARAM_CODEBASE, WeasisConfig.getCodebase(request, props, false));
-                addElementWithNullValue(buf, WeasisConfig.PARAM_CODEBASE_EXT, WeasisConfig.getCodebase(request, props, true));
 
-                // Add properties
+            // GET weasisConfigUrl FROM REQUEST ARGUMENTS
+            String weasisConfigUrl = request.getParameter(WeasisConfig.PARAM_CONFIG_URL);
+
+            // OR GET weasisConfigUrl FROM CONNECTOR'S CONFIGURATION
+            if (weasisConfigUrl == null) {
+                weasisConfigUrl = props.getProperty(SERVICE_CONFIG);
+            }
+
+            if (weasisConfigUrl != null) {
+                // ADD weasisConfigUrl URL PARAMETER
+                addElement(buf, WeasisConfig.PARAM_CONFIG_URL, weasisConfigUrl);
+
+            } else {
+                // OR BUILD CUSTOM CONFIG
+
+                addElementWithNullValue(buf, WeasisConfig.PARAM_CODEBASE,
+                    WeasisConfig.getCodebase(request, props, false));
+                addElementWithNullValue(buf, WeasisConfig.PARAM_CODEBASE_EXT,
+                    WeasisConfig.getCodebase(request, props, true));
+
+                // GET PROPERTIES PARAMETERS FROM REQUEST ARGUMENTS
                 Map<String, String[]> params = request.getParameterMap();
                 Map<String, String> properties = getPropertiesFromRequestParameters(params);
+
+                // PREFERENCE SERVICE URL IS SET FROM CONNECTOR'S CONFIGURATION IF NOT PROVIDED BY REQUEST
                 if (properties.get(SERVICE_PREFS) == null) {
                     String prefs = props.getProperty(SERVICE_PREFS);
                     if (StringUtil.hasText(prefs)) {
-                        // Add the preference service URL from this component configuration
                         properties.put(SERVICE_PREFS, prefs);
                     }
                 }
 
+                // ADD PROPERTIES PARAMETERS
                 for (Entry<String, String> entry : properties.entrySet()) {
                     StringBuilder b = new StringBuilder(entry.getKey());
                     b.append(' ');
@@ -120,29 +137,31 @@ public class GetWeasisProtocol extends HttpServlet {
                     addElement(buf, WeasisConfig.PARAM_PROPERTY, b.toString());
                 }
 
-                // Add arguments
+                // ADD ARGUMENTS PARAMETERS
                 handleRequestParameters(buf, params, WeasisConfig.PARAM_ARGUMENT);
-            } else {
-                addElement(buf, WeasisConfig.PARAM_CONFIG_URL, urlCfg);
             }
+
+            // ADD AUTHORIZATION PARAMETERS
             addElement(buf, WeasisConfig.PARAM_AUTHORIZATION, ServletUtil.getAuthorizationValue(request));
 
+            // BUILD LAUNCH URL
             StringBuilder wurl = new StringBuilder("weasis://");
             wurl.append(URLEncoder.encode(buf.toString(), "UTF-8"));
+
             response.sendRedirect(wurl.toString());
         } catch (Exception e) {
             LOGGER.error("Redirect to weasis secheme", e);
             ServletUtil.sendResponseError(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
-    
+
     private static void addElementWithNullValue(StringBuilder buf, String key, String val) {
         buf.append(' ');
         buf.append(key);
         if (StringUtil.hasText(val)) {
             buf.append("=\"");
             buf.append(val);
-            buf.append("\"");  
+            buf.append("\"");
         }
     }
 
