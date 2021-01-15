cb63cd48011a28626ff6643cfc7c9e3a0ddb4b68
Benoit Jacquemoud <btja@hcuge.ch>
2021-01-15 16:32:06 +0100

    fix licenseHeader date

--- src/main/java/org/weasis/util/InetUtil.java
+++ src/main/java/org/weasis/util/InetUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Weasis Team and other contributors.
+ * Copyright (c) 2020-2021 Weasis Team and other contributors.
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0, or the Apache
